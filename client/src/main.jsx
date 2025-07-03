256e264c51f53b4a04cd1d062dd1dc4fcb27fcbd
AhmadJahanzaib10 <jahanzaibahmad200@gmail.com>
2025-07-03 19:44:23 +0500

    seven

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { BrowserRouter } from 'react-router-dom'; 
 import App from './App.jsx'
 import "./App.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> 
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
